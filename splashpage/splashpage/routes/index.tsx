e8a32530a4753ebc71070a35bf111c917c40afc2
Jason Goldman <106274489+Trablink@users.noreply.github.com>
2022-12-27 11:15:01 -0600

    paying in front end

--- splashpage/splashpage/routes/index.tsx
+++ splashpage/splashpage/routes/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to VS|Code!
+          Welcome to VS|Squirrel!
         </p>
         <Counter start={3} />
       </div>
