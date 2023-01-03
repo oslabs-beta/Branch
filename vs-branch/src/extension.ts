274966f23a44b20e04b7f083eecda2896b1486eb
SirGoatsalot <fostersullivan12@gmail.com>
2023-01-03 14:36:51 -0500

    Clean up boilerplate

--- vs-branch/src/extension.ts
+++ vs-branch/src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as path from 'path';
+import vscode = require('vscode');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
