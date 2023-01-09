4599d6741a0c911a489b5e331bfb7a0c6e0b7f66
Jason Goldman <106274489+Trablink@users.noreply.github.com>
2023-01-09 15:12:00 -0600

    all routes can scrape with no fail, even empty routes

--- vs-branch/src/scraper.js
+++ vs-branch/src/scraper.js
@@ -5,35 +5,42 @@ const child_process_1 = require("child_process");
 const fs = require('fs');
 const path = require('path');
 const scrapedData = [];
+let formattedData;
 const scrape = (cwd, method) => {
     //TODO fix asynchronicity problems, specifically when to return the data retrieved.
     // Ideas: Return a promise, resolve when child process exits
     // run ripgrep in a shell 
     let result = '';
     let rg;
+    console.log(`break point 1`);
     rg = (0, child_process_1.spawn)('rg', [`.${method}`, './'], {
         cwd: cwd + '/server',
     });
+    console.log(`break point 2`);
     rg.stdout.on("data", data => {
+        console.log(`break point 3`);
         console.log(`ripgrep stdout: \n${data}`);
         result = data;
     });
     rg.stderr.on("data", data => {
+        console.log(`break point 4`);
         console.log(`ripgrep stderr: ${data}`);
     });
     rg.on('error', (error) => {
+        console.log(`break point 5`);
         console.log(`ripgrep error: ${error.message}`);
     });
     rg.on("close", code => {
-        console.log(`ripgrep exited with code ${code}`);
-        console.log(`Resolved ${method} result: \n${result}`);
+        console.log(`break point 6`);
+        // console.log(`ripgrep exited with code ${code}`);
+        // console.log(`Resolved ${method} result: \n${result}`);
         scrapedData.push(result.toString());
         console.log(`scrapedData: `, scrapedData);
         // console.log(`fs:  `, fs);
         // const fullPath = path.resolve(__dirname, 'test.ts');
         // console.log('FULL PATH: ', fullPath);
         // fs.writeFileSync(fullPath, result, {"flag": "a+"});
-        if (scrapedData.length > 3) {
+        if (scrapedData.length > 4) {
             format(scrapedData);
         }
     });
@@ -45,7 +52,7 @@ const getRoutes = async (cwd) => {
     const finalData = {};
     const getResult = await scrape(cwd, 'get');
     const putResult = await scrape(cwd, 'post');
-    // const patchResult = await scrape(cwd, 'patch');
+    const patchResult = await scrape(cwd, 'patch');
     const postResult = await scrape(cwd, 'put');
     const deleteResult = await scrape(cwd, 'delete');
 };
--- vs-branch/src/scraper.ts
+++ vs-branch/src/scraper.ts
@@ -6,6 +6,14 @@ const fs = require('fs');
 const path = require('path');
 
 const scrapedData: string[] = [];
+let formattedData: RouteTree;
+
+type RouteTree = {
+  name: string;
+  method?: string[];
+  parent: string | null;
+  children: RouteTree[] | null;
+};
 
 const scrape = (cwd: string, method: string) => {
   //TODO fix asynchronicity problems, specifically when to return the data retrieved.
@@ -13,34 +21,40 @@ const scrape = (cwd: string, method: string) => {
   // run ripgrep in a shell 
   let result: string = '';
   let rg;
+  console.log (`break point 1`);
   rg = spawn('rg', [`.${method}`, './'],
     { 
       cwd : cwd + '/server', 
     });
+    console.log (`break point 2`);
 
   rg.stdout.on("data", data => {
+    console.log (`break point 3`);
     console.log(`ripgrep stdout: \n${data}`);
     result = data;
   });
   
   rg.stderr.on("data", data => {
+    console.log (`break point 4`);
       console.log(`ripgrep stderr: ${data}`);
   });
   
   rg.on('error', (error) => {
+    console.log (`break point 5`);
       console.log(`ripgrep error: ${error.message}`);
   });
   
   rg.on("close", code => {
-      console.log(`ripgrep exited with code ${code}`);
-      console.log(`Resolved ${method} result: \n${result}`);
+    console.log (`break point 6`);
+      // console.log(`ripgrep exited with code ${code}`);
+      // console.log(`Resolved ${method} result: \n${result}`);
       scrapedData.push(result.toString());
       console.log (`scrapedData: `, scrapedData);
       // console.log(`fs:  `, fs);
       // const fullPath = path.resolve(__dirname, 'test.ts');
       // console.log('FULL PATH: ', fullPath);
       // fs.writeFileSync(fullPath, result, {"flag": "a+"});
-      if(scrapedData.length > 3) {
+      if(scrapedData.length > 4) {
         format(scrapedData);
       }
   });
@@ -54,7 +68,7 @@ const getRoutes = async (cwd: string) => {
   const finalData = {};
   const getResult = await scrape(cwd, 'get');
   const putResult = await scrape(cwd, 'post');
-  // const patchResult = await scrape(cwd, 'patch');
+  const patchResult = await scrape(cwd, 'patch');
   const postResult = await scrape(cwd, 'put');
   const deleteResult = await scrape(cwd, 'delete');
   
